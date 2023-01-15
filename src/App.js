fe2cd5d6155bc270488cc72ad5e0ebe4510306b8
Caleb Mantey <manteycaleb@gmail.com>
2023-01-15 16:48:57 +0000

    socket client side code with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:4000");
 
 function App() {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      socket.on("message", (data) => {
+        console.log(data);
+        const new_messages = messages;
+
+        if (
+          new_messages.find(
+            (message) =>
+              message.msg === data.msg && message.username === data.username
+          )
+        )
+          return;
+
+        new_messages.push(data);
+
+        setMessages(() => [...new_messages]);
+      });
+      console.log(socket.connected); // true
+    });
+
+    // return () => {
+    //   socket.on("disconnect", () => {
+    //     console.log(socket.connected); // false
+    //   });
+    // };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "1px solid red", padding: 40 }}>
+        <h4>Username</h4>
+        <div>
+          <input
+            name="username"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+          />
+        </div>
+        <h4>Message</h4>
+        <div>
+          <input
+            name="message"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+            type="text"
+          />
+          <br />
+          <br />
+          <button
+            onClick={() => {
+              if (message === "" || name === "") {
+                return;
+              }
+              socket.emit("message", {
+                msg: message,
+                username: name,
+              });
+            }}
+          >
+            Send
+          </button>
+        </div>
+      </div>
+      <div style={{ border: "1px solid blue", padding: 40 }}>
+        <h3 style={{ margin: 0 }}>Messages</h3>
+        <div>
+          {messages.map((message, index) => (
+            <div key={index}>
+              <h5>{message.username}</h5>
+              <p> {message.msg}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
